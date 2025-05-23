345385d60419a6ae379c178ed1cfd914fffbb3dd
Emily Diaz <emilmdiaz@gmail.com>
2025-05-23 13:47:50 -0400

    added more organization to the files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let resumebttn=document.getElementById("resume");
 let imgdefault=document.getElementById("imgdefault");
 let imghover=document.getElementById("imghover");
+let boozallenbutton=document.getElementById("boozallenbttn")
 
 
 
@@ -12,4 +13,8 @@ resumebttn.addEventListener("mouseover",()=>{
 resumebttn.addEventListener("mouseleave",()=>{
     imgdefault.hidden=false;
     imghover.hidden=true;
+})
+
+boozallenbutton.addEventListener("click",()=>{
+   location.replace('BoozAllenCaseStudy.html')
 })
