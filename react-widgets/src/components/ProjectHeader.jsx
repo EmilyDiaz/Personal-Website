8fed50133b1a881dbad71b92a0cfbb2a68a04127
Emily Diaz <emilmdiaz@gmail.com>
2026-06-12 12:27:16 -0400

    react fixes

--- react-widgets/src/components/ProjectHeader.jsx
+++ react-widgets/src/components/ProjectHeader.jsx
@@ -1,7 +1,7 @@
 export default function ProjectHeader(props) {
     return (
         <>
-            {props.projecttitleimg? <img src={props.projecttitleimg}></img>:null}
+            {props.projecttitleimg? <img width="100%" src={props.projecttitleimg}></img>:null}
             {props.projectname? <h1>{props.projectname}</h1>:null}
             <section className="projectimage">
                 <img src={props.projectimg} alt={props.alt}></img>
