f685c8e1ef474ae91b6ad615ae23e2046c6563e8
Emily Diaz <emilmdiaz@gmail.com>
2026-06-12 11:10:06 -0400

    Enhancements V2 with react build

--- react-widgets/src/components/Footer.jsx
+++ react-widgets/src/components/Footer.jsx
@@ -45,13 +45,13 @@ export default function Footer() {
 
     return (
         <>
-            <div id="graphicsRight">
+            <div id="graphicsLeft">
                 <canvas id="hexCanvas" tabIndex="0"></canvas>
                 <canvas id="cashewCanvas" tabIndex="0"></canvas>
             </div>
             <p tabIndex="0">This website was designed & coded by Emily. Two gremlins named Hex and Cashew supervised,
                 though.</p>
-            <div id="graphicsLeft">
+            <div id="graphicsRight">
                 <a href="https://github.com/EmilyDiaz" target="_blank" rel="noopener noreferrer"><img
                     src="Images/Github.svg" alt=""></img></a>
                 <a href="https://www.linkedin.com/in/emily-diaz-b5b696203" target="_blank" rel="noopener noreferrer"><img
