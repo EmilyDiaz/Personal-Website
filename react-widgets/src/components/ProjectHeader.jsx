05e9ed8a3fe6b7a55bd9ce12227cee159f30a507
Emily Diaz <emilmdiaz@gmail.com>
2026-07-29 10:17:02 -0400

    fix react components

--- react-widgets/src/components/ProjectHeader.jsx
+++ react-widgets/src/components/ProjectHeader.jsx
@@ -1,10 +1,12 @@
+import { resolveSitePath } from '../sitePaths'
+
 export default function ProjectHeader(props) {
     return (
         <>
-            {props.projecttitleimg? <img width="100%" src={props.projecttitleimg}></img>:null}
+            {props.projecttitleimg? <img width="100%" src={resolveSitePath(props.projecttitleimg)}></img>:null}
             {props.projectname? <h1>{props.projectname}</h1>:null}
             <section className="projectimage">
-                <img src={props.projectimg} alt={props.alt}></img>
+                <img src={resolveSitePath(props.projectimg)} alt={props.alt}></img>
             </section>
         </>
     )
